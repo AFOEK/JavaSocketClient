d1307261094f1d45c9e5ed92b3d754ce2eb8beb2
Tri <nitylims@gmail.com>
2020-12-04 21:52:10 +0700

    declare var (Java)

--- app/src/main/java/com/example/socketclient/MainActivity.java
+++ app/src/main/java/com/example/socketclient/MainActivity.java
@@ -6,12 +6,26 @@ import java.net.*;
 import java.io.File;
 import java.io.IOException;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt_ip, txt_port, txt_msg, txt_history;
+    Button butt_connect, butt_dc, butt_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_ip = (EditText)findViewById(R.id.txt_ip);
+        txt_port = (EditText)findViewById(R.id.txt_port);
+        txt_msg = (EditText)findViewById(R.id.txt_msg);
+        txt_history = (EditText)findViewById(R.id.txt_history);
+
+        butt_connect = (Button)findViewById(R.id.butt_connect);
+        butt_dc = (Button)findViewById(R.id.butt_dc);
+        butt_send = (Button)findViewById(R.id.butt_send);
     }
 }
